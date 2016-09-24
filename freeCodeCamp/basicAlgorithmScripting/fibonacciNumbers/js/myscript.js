c4beccd58090644b8dafe10d314e661834560a88
salasy <sasa.kraljevic@gmail.com>
2016-09-24 11:19:25 +0200

    Adding functions for odd and even array

--- freeCodeCamp/basicAlgorithmScripting/fibonacciNumbers/js/myscript.js
+++ freeCodeCamp/basicAlgorithmScripting/fibonacciNumbers/js/myscript.js
@@ -1,26 +1,52 @@
 $(document).ready(function(){
 
-  function sumFibs(num) {
+  function createFibonacci(num) {
   // get fibonacci sequence array
-  var fibonacciArray = [1];
-  var num = $("#fibonacci").val();
-  for (var i = 1; i<=num; i = (i + fibonacciArray[fibonacciArray.length-2]) ) {
+    var fibonacciArray = [1];
+    var num = $("#fibonacci").val();
+    for (var i = 1; i<=num; i = (i + fibonacciArray[fibonacciArray.length-2]) ) {
+      fibonacciArray.push(i);
+    }
+  $("#results").html(fibonacciArray + ',');
+  };
+  $("#fibArray").click(createFibonacci);
+//////////////////////////////////////////////
+
+  function createEvens() {
+  // push evens to other array
+    var evenArray = [];
+    var fibonacciArray = [1];
+    var num = $("#fibonacci").val();
+    for (var i = 1; i<=num; i = (i + fibonacciArray[fibonacciArray.length-2]) ) {
     fibonacciArray.push(i);
-  }
-  
-  // choose what sum you would like to return
-  //return fibonacciArray;
+    }
+    fibonacciArray.forEach(function(elem) {
+      // if element is even number, push it to evenArray
+      if (elem % 2 === 0) {
+        evenArray.push(elem);
+      }
+    })
+  $("#results").html(evenArray + ',');
+  };
+  $("#evenArray").click(createEvens);
+//////////////////////////////////////////////
 
-  $("#roman").html(fibonacciArray + ',');
+  function createOdds() {
+  // push odds to other array
+    var oddArray = [];
+    var fibonacciArray = [1];
+    var num = $("#fibonacci").val();
+    for (var i = 1; i<=num; i = (i + fibonacciArray[fibonacciArray.length-2]) ) {
+    fibonacciArray.push(i);
+    }
+    fibonacciArray.forEach(function(elem2) {
+      // if element is odd number, push it to evenArray
+      if (elem2 % 2 !== 0) {
+        oddArray.push(elem2);
+      }
+    })
+  $("#results").html(oddArray + ',');
+  };
+  $("#oddArray").click(createOdds);
 
-};
-
-
-$("#fibArray").click(sumFibs);
-$("#fibonacci").on('keyup', function(e){
-  console.log(e.which);
-  if(e.which=='13'){
-    sumFibs();
-  }
-});
 });
