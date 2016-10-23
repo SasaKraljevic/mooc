ce3ac1fc88893bb1fed4416162ef6c36cb02e447
salasy <sasa.kraljevic@gmail.com>
2016-10-23 11:33:59 +0200

    finally :)

--- freeCodeCamp/advancedAlgorithmScripting/03_exactChange.js
+++ freeCodeCamp/advancedAlgorithmScripting/03_exactChange.js
@@ -1,3 +1,17 @@
+/*
+Design a cash register drawer function checkCashRegister() that accepts purchase price as 
+the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) 
+as the third argument.
+
+cid is a 2D array listing available currency.
+Return the string "Insufficient Funds" if cash-in-drawer is less than the change due. 
+Return the string "Closed" if cash-in-drawer is equal to the change due.
+Otherwise, return change in coin and bills, sorted in highest to lowest order.
+
+Here are some helpful links:
+Global Object
+*/
+
 function checkCashRegister(cijena, gotovina, novacUBlagajni) {
 
   var kusur = gotovina - cijena;
@@ -18,20 +32,19 @@ function checkCashRegister(cijena, gotovina, novacUBlagajni) {
     ["NICKEL", 0.05],
     ["PENNY", 0.01] 
   ];
-
- 
+  
   // step 02: koliko je ukupno novca u blagajni??
   var cashInRegister = function(arr) { 
       for ( var i in arr ) {
         total += arr[i][1];
       }
     if ( total === kusur ) {
-      console.log("Closed");
+      return "Closed";
     }
     if ( total < kusur ) {
-      console.log("Insufficient Funds");
+      return "Insufficient Funds";
     }
-    return total.toFixed(2);
+   // return total.toFixed(2);
   };
   cashInRegister(novacUBlagajni);
 
@@ -49,24 +62,48 @@ function checkCashRegister(cijena, gotovina, novacUBlagajni) {
       potroseno += novcanice[i][1];
       potroseno = parseFloat(potroseno.toFixed(2));
       console.log("POTROSENO: ", parseFloat(potroseno.toFixed(2)));  
-    }
+    }  // end of while loop
+    
     if ( potroseno > 0 ) {
       rezultat.push([novcanice[i][0], parseFloat(potroseno.toFixed(2))]);
     }
-    if ( potroseno === total ) {
+    if ( potroseno === total && kusur < potroseno) {
       return "Closed";
     }
-}  
-  if (rezultat.length < 1 || kusur > 0) {
+}  // end of for loop
+  
+  if (rezultat.length < 1 || kusur > 0 ) {
     return "Insufficient Funds";
   }
+
   console.log(rezultat);
   return rezultat;
-  
-  
-  
 }
-
-
 checkCashRegister(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]);
 //should return [["TWENTY", 60.00], ["TEN", 20.00], ["FIVE", 15.00], ["ONE", 1.00], ["QUARTER", 0.50], ["DIME", 0.20], ["PENNY", 0.04]]
+
+/*
+checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]) 
+should return an array.
+
+checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) 
+should return a string.
+
+checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) 
+should return a string.
+
+checkCashRegister(19.50, 20.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]) 
+should return [["QUARTER", 0.50]].
+
+checkCashRegister(3.26, 100.00, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.10], ["QUARTER", 4.25], ["ONE", 90.00], ["FIVE", 55.00], ["TEN", 20.00], ["TWENTY", 60.00], ["ONE HUNDRED", 100.00]]) 
+should return [["TWENTY", 60.00], ["TEN", 20.00], ["FIVE", 15.00], ["ONE", 1.00], ["QUARTER", 0.50], ["DIME", 0.20], ["PENNY", 0.04]].
+
+checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) 
+should return "Insufficient Funds".
+
+checkCashRegister(19.50, 20.00, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1.00], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) 
+should return "Insufficient Funds".
+
+checkCashRegister(19.50, 20.00, [["PENNY", 0.50], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) 
+should return "Closed".
+*/
