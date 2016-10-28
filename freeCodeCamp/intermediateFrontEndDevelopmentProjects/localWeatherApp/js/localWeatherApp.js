28d3b132282bf78fcb6a6bbba184689d4c3952d2
salasy <sasa.kraljevic@gmail.com>
2016-10-28 23:43:59 +0200

    Adding pressure

--- freeCodeCamp/intermediateFrontEndDevelopmentProjects/localWeatherApp/js/localWeatherApp.js
+++ freeCodeCamp/intermediateFrontEndDevelopmentProjects/localWeatherApp/js/localWeatherApp.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 $.get("http://ipinfo.io", function (response) {
     //$("#ip").html("IP: " + response.ip);
     city = response.city;
-    console.log(response);
+    console.log(response.loc);
     $("#city").html("Location: " + response.city + ", " + response.region);
     //$("#city2").html(JSON.stringify(response, null, 4));
 }, "jsonp")
@@ -15,14 +15,16 @@ $.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=8c0fc
     console.log("--------------");
     console.log(val);
     var weather = val.weather[0].main;
-    var tempC = Math.floor(val.main.temp);
+    var tempC = Math.floor(val.main.temp) + " &#8451;";
     var tempF = Math.floor((tempC*1.8)+32); 
     var icon = val.weather[0].icon;
     var wind = val.wind.speed;
+    var pressure = val.main.pressure + " hPa";
      
-    $("#temp").html(tempC + " &#8451;"); 
+    $("#temp").html(tempC); 
     $("#humidity").html(weather); 
     $("#wind").html(wind + " km/h"); 
+    $("#pressure").html(pressure);
      
     // $("#temperature").html(html3);
 
