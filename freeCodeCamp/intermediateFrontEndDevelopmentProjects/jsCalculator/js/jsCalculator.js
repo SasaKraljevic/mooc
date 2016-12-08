d4b27498e7beac3247c3a6406d7244580b037e3c
salasy <sasa.kraljevic@gmail.com>
2016-12-09 00:19:39 +0100

    issue with multiple decimal points

--- freeCodeCamp/intermediateFrontEndDevelopmentProjects/jsCalculator/js/jsCalculator.js
+++ freeCodeCamp/intermediateFrontEndDevelopmentProjects/jsCalculator/js/jsCalculator.js
@@ -1,21 +1,36 @@
+var currValue = "";
+var value = "";
+
 $(document).ready(function(){
   var on = false;
   var off = true;
 ///////////////////////////////////////
   
-var currValue = "";
+
 $('button').on('click', function () {
   
-  var value = this.id;
-  console.log(value);
-  currValue += value;
-  console.log(currValue);
+  value = this.id;
+  console.log("value :", value);
+  if(this.id !== 'result' && this.id !== 'c') {
+    currValue += value;
+    console.log(currValue);
+  }
   $("input").attr('value', currValue);
 });
 
 // C button function
 $("#c").on("click", function() {
   value = 0;
+  currValue = "";
+  $("input").attr('value', value);
+  console.log("TEST :", value);
+  //$("input").attr('value', value);
+});
+  
+// result button function
+$("#result").on("click", function() {
+  value = eval(currValue);
+  currValue = eval(currValue);
   $("input").attr('value', value);
 });
 
