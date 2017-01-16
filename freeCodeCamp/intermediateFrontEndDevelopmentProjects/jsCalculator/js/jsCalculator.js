23685d9094f11573e22194e799b5a0e43e85366e
salasy <sasa.kraljevic@gmail.com>
2017-01-16 18:08:09 +0100

    Cleaning js code

--- freeCodeCamp/intermediateFrontEndDevelopmentProjects/jsCalculator/js/jsCalculator.js
+++ freeCodeCamp/intermediateFrontEndDevelopmentProjects/jsCalculator/js/jsCalculator.js
@@ -1,72 +1,124 @@
-var currValue = "";
-var value = "";
-
 $(document).ready(function(){
   $('button').not('.onoff').prop('disabled', true);
-  var on = false;
-  var off = true;
-///////////////////////////////////////
-//$('input').click(function ()  {
-$('.onoff').click(function ()  {
-  var clicks = $(this).data('clicks');
-  
-  if (clicks) {
-    $('button').not('.onoff').prop('disabled', true);
-    //$('input').prop('value', '');
-    currValue = '';
-    $("input").attr('value', currValue);
-    
+
+  // turn it on and off
+  $('.onoff').click(function ()  {
+    var clicks = $(this).data('clicks');
+    if (clicks) {
+      $('button').not('.onoff').prop('disabled', true);
+      currValue = '';
+      $("#display").text(currValue);
     } else {
-    $('button').not('.onoff').prop('disabled', false);
-    currValue = '0';
-    $("input").attr('value', currValue);
+        $('button').not('.onoff').prop('disabled', false);
+        currValue = '0';
+        $("#display").text(currValue);
+      } 
+    $(this).data("clicks", !clicks);
+  }); // /.turn on and off
+  
+  // test number length
+  var testNumLength = function(number) {
+    if (number.length > 18) {
+      number = "";
+      $("#display").text("18 digits calculator");
     } 
-    
-  $(this).data("clicks", !clicks);
+  }; //.test number length
+  
+  // calculation
+  function calculate(a, b, operator) {
+    switch (operator) {
+      case "+":
+        return Number(a) + Number(b);
+      case "-":
+        return Number(a) - Number(b);
+      case "*":
+        return Number(a) * Number(b);
+      case "/":
+        return Number(a) / Number(b);
+    }
+  } //. calculation
+  
+  // display numbers
+  var number = '';
+  var newNumber = '';
+  var operator = '';
+  var total = '';
+  //var temp = '';
+  $(".numButton").not("#clear, #decimal").click(function(){
+    total = '';
+    //newNumber = total;
+    number += $(this).text();
+    number = number.replace(/^0+/, ''); // number can't start with zero
+    number = number.replace(/^[\+\-\*\/]/, ''); // can't start with operators
+    $("#display").text(number);
+    testNumLength(number);
+  });
+  
+  // decimal numbers
+  $("#decimal").click(function(){
+    if (!number.match(/[.]/)) {
+      number += $(this).text();
+      $("#display").text(number);
+      testNumLength(number);
+    } 
   });
 
-   
-$('button').on('click', function () {
+  // display operators
+  $(".operator").not("#equals, #decimal").click(function(){
   
-  value = this.id;
-  console.log("value :", value);
-  if(this.id !== 'result' && this.id !== 'c') {
-    //currValue += value;
-    
-    ///////////////////
-    // allow only one decimal point
-    if(this.id === '.') {
-      if (currValue.indexOf('.') === -1) {
-        currValue += value;
+    if( operator !== '' ) {
+      operator = $(this).text();
+      $("#display").text(operator);
+      total = calculate(newNumber, number, operator);
+      newNumber = total;
+      number = '';
+      operator = '';
+      operator = $(this).text();
+      $("#display").text(operator);
+    } else {
+        operator = $(this).text();
+        $("#display").text(operator);
+        if(total !== '') {
+          newNumber = total;
+          number = '';
+          //operator = '';
+        } else {
+            newNumber = number; // number
+            number = "";
+            operator = $(this).text();
+            $("#display").text(operator);
+          }
       }
-    }
-    else {
-      currValue += value;
-    }
-    ///////////////////
-
-    console.log("!!!!! :", currValue.length);
-  }
+  });
   
-  $("input").attr('value', currValue);
-});
+  // C button
+  $("#clear").click(function(){
+    number = "";
+    newNumber = '';
+    total = '';
+    operator = '';
+    $("#display").text("0");
+  });
 
-// C button function
-$("#c").on("click", function() {
-  value = 0;
-  currValue = "";
-  $("input").attr('value', value);
-  console.log("TEST :", value);
-  //$("input").attr('value', value);
-});
   
-// result button function
-$("#result").on("click", function() {
-  value = eval(currValue);
-  currValue = eval(currValue);
-  $("input").attr('value', value);
-});
-
-
+  // calculate
+  $("#equals").click(function(){
     
-});
+    total = calculate(newNumber, number, operator);
+    
+    if (total % 1 !== 0) {
+      total = total.toFixed(4);
+      $("#display").text(total);
+      newNumber = total;
+      number = '';
+      operator = '';
+    } else {
+      $("#display").text(total);
+      newNumber = total;
+      number = '';
+      operator = '';
+      }
+  });
+  
+  
+}); //.main function
