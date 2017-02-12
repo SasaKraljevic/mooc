c154e43c9afb176a72a4e8b6117090c7e037b2af
salasy <sasa.kraljevic@gmail.com>
2017-02-12 23:44:09 +0100

    Trying to get users data

--- freeCodeCamp/intermediateFrontEndDevelopmentProjects/twitchTv/js/twitch2.js
+++ freeCodeCamp/intermediateFrontEndDevelopmentProjects/twitchTv/js/twitch2.js
@@ -27,8 +27,12 @@ $(document).ready(function() {
   var offlineUsers = [];
 
   var twitchLogo = 'https://news.bitcoin.com/wp-content/uploads/2016/09/Twitch-TV-logo-300x300.png';
-
+      var userName;
+      var userUrl;
+      var userLogo;
+      var userStatus;
   users.forEach(function(user) {
+
     $.getJSON(baseUrl + 'streams/' + user + '?callback=?&client_id=' + clientId, function(streamData) {
       // get self url and channel url
       // console.log("_LINKS :", streamData._links);
@@ -36,70 +40,38 @@ $(document).ready(function() {
       //console.log("NAME :", streamData.stream.channel.display_name);
 
       // check is offline?
-      if (streamData.stream === null || streamData.stream === undefined) {
-        $.getJSON(baseUrl + 'channels/' + user + '?callback=?&client_id=' + clientId, function(channelData) {
-          console.log("CHANNEL DATA :", channelData)
-          console.log("OFFLINE USERNAME :", channelData.display_name);
-          console.log("OFFLINE USERNAME LINK :", channelData.url);
-          console.log("OFFLINE USER LOGO :", channelData.logo);
+//      if (streamData.stream === null) {
+          //console.log("CHANNEL DATA :", channelData)
+          userName = streamData.display_name; 
+          //console.log("OFFLINE USERNAME :", channelData.display_name);
+          userGame = streamData.game;
+          //console.log("GAME :", channelData.game);
+          //console.log("OFFLINE USERNAME LINK :", channelData.url);
+          //console.log("OFFLINE USER LOGO :", channelData.logo);
           // status could be a null, so I need to put something instead
-          console.log("OFFLINE USER STATUS :", channelData.status);
+          //console.log("OFFLINE USER STATUS :", channelData.status);
 
-          $('#content').append('\
-            <div class="col-sm-6 col-md-4">\
-              <div class="thumbnail">\
-                <a href="">\
-                  <img src="..." alt="...">\
-                </a>\
-                <div class="caption">\
-                  <h3></h3>\
-                  <h4></h4>\
-                  <p class="status">...</p>\
-                  <span class="label label-success">Online</span>\
-                </div>\
-              </div>\
-            </div>');
-
-          $('h3').html(channelData.display_name);
-          //$('h4').html(userGame);
-          //$('.status').html(userStatus);
-          //$('img').attr('src', userLogo);
-          //$('a').attr('href', userUrl);
-          //offlineUsers.push(channelData);
-        });
-        //console.log("OFFLINE USER :", user);
-        //console.log("OFFLINE USER LINK:", 'https://www.twitch.tv/' + user);
-        //console.log("STREAM DATA :", streamData._links.channel);
-        // if user is offline, we need data from /channel/
-      /*   $.getJSON(baseUrl + 'channel/' + user + '?callback=?&client_id=' + clientId, function(channelData) {
-          console.log("CHANNEL DATA :", channelData)
-
-          })
-         console.log("#################");
-*/
-
-      }
-
-      //var userName = streamData.stream.channel.display_name;
-      //var userGame = streamData.stream.channel.game;
-      //var userLogo = streamData.stream.channel.logo;
-      //var userBanner = streamData.stream.preview.medium;
-      //var userStatus = streamData.stream.channel.status;
-      //var userUrl = streamData.stream.channel.url;
-      //var userViewers = streamData.stream.viewers;
-      //console.log("ONLINE :", streamData);
-      //console.log("NAME :", userName);
-      //console.log("GAME :", userGame);
-      //console.log("LOGO :", userLogo);
-      //console.log("BANNER :", userBanner);
-      //console.log("STATUS :", userStatus);
-      //console.log("URL :", userUrl);
-      //console.log("VIEWERS :", userViewers);  
-      console.log("------------------------"); 
-        
+  console.log("----------", streamData.stream.channel.display_name);
+  $('#content').append('\
+    <div class="col-sm-6 col-md-4">\
+      <div class="thumbnail">\
+        <a href="">\
+          <img src="..." alt="...">\
+        </a>\
+        <div class="caption">\
+          <h3></h3>\
+          <h4></h4>\
+          <p class="status">...</p>\
+          <span class="label label-success">Online</span>\
+        </div>\
+      </div>\
+    </div>');
+      
+    $('h3').html(userName);
+    $('h4').html(userGame);   
+   
     });
 
-    });
-//console.log("OFFLINE USERS ARRAY", offlineUsers);
-  
+});
+
 });
