ae66bf8a0291830cf77210cda2afac226af69dfe
salasy <sasa.kraljevic@gmail.com>
2017-02-14 21:41:07 +0100

    Aproach with functions

--- freeCodeCamp/intermediateFrontEndDevelopmentProjects/twitchTv/js/twitch2.js
+++ freeCodeCamp/intermediateFrontEndDevelopmentProjects/twitchTv/js/twitch2.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+//$(document).ready(function() {
 
   var baseUrl = 'https://wind-bow.gomix.me/twitch-api/';
   var clientId = 'k4y0o278wsctmzrems5r33iiw7m923v';
@@ -21,57 +21,75 @@ $(document).ready(function() {
         "behkuhtv",
         "fakename",
         "food"
-  ];
-  var allUsers = [];
-  var onlineUsers = [];
-  var offlineUsers = [];
+        ];
 
   var twitchLogo = 'https://news.bitcoin.com/wp-content/uploads/2016/09/Twitch-TV-logo-300x300.png';
-      var userName;
-      var userUrl;
-      var userLogo;
-      var userStatus;
+  
+  // get stream data
+  function getStreamData(users) {
+
+
   users.forEach(function(user) {
+    var userName;
+    var userFollowers;
+    var userGame;
+    var userLogo;
+    var userStatus;
+    var userUrl;
+    
+  $.getJSON(baseUrl + 'streams/' + user + '?callback=?&client_id=' + clientId, function(streamData) {
 
-    $.getJSON(baseUrl + 'streams/' + user + '?callback=?&client_id=' + clientId, function(streamData) {
-      // get self url and channel url
-      // console.log("_LINKS :", streamData._links);
-      // get stream data
-      //console.log("NAME :", streamData.stream.channel.display_name);
+    //console.log("streamData :", streamData);
 
-      // check is offline?
-//      if (streamData.stream === null) {
-          //console.log("CHANNEL DATA :", channelData)
-          userName = streamData.display_name; 
-          //console.log("OFFLINE USERNAME :", channelData.display_name);
-          userGame = streamData.game;
-          //console.log("GAME :", channelData.game);
-          //console.log("OFFLINE USERNAME LINK :", channelData.url);
-          //console.log("OFFLINE USER LOGO :", channelData.logo);
-          // status could be a null, so I need to put something instead
-          //console.log("OFFLINE USER STATUS :", channelData.status);
+    // get channels data
+    userName = streamData.stream.channel.display_name;
+    //console.log("userName :", userName);
+    userFollowers = streamData.stream.channel.followers;
+    //console.log("userFollowers :", userFollowers); 
+    userGame = streamData.stream.channel.game;
+    //console.log("userGame :", userGame);
+    userLogo = streamData.stream.channel.logo;
+    //console.log("userLogo :", userLogo);
+    userStatus = streamData.stream.channel.status;
+    //console.log("userStatus :", userStatus);
+    userUrl = streamData.stream.channel.url;
+    //console.log("userUrl :", userUrl);
+    //console.log("channelData :", channelData);
 
-  console.log("----------", streamData.stream.channel.display_name);
-  $('#content').append('\
+    console.log("------------------");
+    console.log("userName :", userName);
+    console.log("userFollowers :", userFollowers); 
+    console.log("userGame :", userGame);
+    console.log("userLogo :", userLogo);
+    console.log("userStatus :", userStatus);
+    console.log("userUrl :", userUrl);
+  }); // end of users.forEach
+  });
+}
+
+getStreamData(users);
+
+/*
+function createHtml() {
+      $('#content').append('\
     <div class="col-sm-6 col-md-4">\
-      <div class="thumbnail">\
-        <a href="">\
-          <img src="..." alt="...">\
-        </a>\
+    <div class="thumbnail">\
+    <a href="">\
+      <img src="..." alt="...">\
+    </a>\
         <div class="caption">\
           <h3></h3>\
           <h4></h4>\
           <p class="status">...</p>\
           <span class="label label-success">Online</span>\
         </div>\
-      </div>\
+    </div>\
     </div>');
-      
+
     $('h3').html(userName);
-    $('h4').html(userGame);   
-   
-    });
-
-});
-
-});
+    $('h4').html(userGame);
+    $('.status').html(userStatus);
+    $('img').attr('src', userLogo);
+    $('a').attr('href', userUrl);
+}*/
+//}); // end of docuent.ready
