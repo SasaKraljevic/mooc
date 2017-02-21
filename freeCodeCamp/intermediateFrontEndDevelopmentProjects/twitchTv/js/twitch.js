adb4221ddc753e0815b9486f7560b7a7c70bb796
salasy <sasa.kraljevic@gmail.com>
2017-02-21 23:24:09 +0100

    Changing online anf offline funtions

--- freeCodeCamp/intermediateFrontEndDevelopmentProjects/twitchTv/js/twitch.js
+++ freeCodeCamp/intermediateFrontEndDevelopmentProjects/twitchTv/js/twitch.js
@@ -1,86 +1,126 @@
-$(document).ready(function() {
+const streamsUrl = 'https://api.twitch.tv/kraken/streams/';
+const channelsUrl = 'https://api.twitch.tv/kraken/channels/';
+const clientId = '?callback=?&client_id=k4y0o278wsctmzrems5r33iiw7m923v';
+const users = [
+    "ESL_SC2",
+    "OgamingSC2",
+    "cretetion",
+    "freecodecamp",
+    "storbeck",
+    "habathcx",
+    "RobotCaleb",
+    "noobs2ninjas",
+    "pink_sparkles",
+    "comster404",
+    "brunofin",
+    "medrybw",
+    "monster",
+    "aces_tv",
+    "loserfruit",
+    "behkuhtv",
+    "fakename",
+    "food"
+    ];
+const noLogo = 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/No_sign.svg/300px-No_sign.svg.png'
 
-  var baseUrl = 'https://crossorigin.me/https://api.twitch.tv/kraken/';
-  var clientId = 'k4y0o278wsctmzrems5r33iiw7m923v';
-  var users = [
-        "ESL_SC2",
-        "OgamingSC2",
-        "cretetion",
-        "freecodecamp",
-        "storbeck",
-        "habathcx",
-        "RobotCaleb",
-        "noobs2ninjas",
-        "pink_sparkles",
-        "comster404",
-        "brunofin",
-        "medrybw",
-        "monster",
-        "aces_tv",
-        "loserfruit",
-        "behkuhtv",
-        "fakename",
-        "food"
-  ];
-  var allUsers = [];
-  var onlineUsers = [];
-  var offlineUsers = [];
-  var twitchLogo = 'https://news.bitcoin.com/wp-content/uploads/2016/09/Twitch-TV-logo-300x300.png';
+function getOfflineData() {
+  $('#content').empty();
+  users.forEach(function(user) {
 
-  for(var i = 0; i < users.length; i++) {
-    // Temporary object
-    var obj = {};
+  $.getJSON(streamsUrl + user + clientId).done(function(streamsData) {
+    //console.log(streamsData);
+    var userName;
+    var userLogo;
+    var userStatus;
+    var userGame;
+    var userUrl;
+    var spanClass;
+    var btnLabel;
 
-
-      $.getJSON(baseUrl + 'streams/' + users[i] + '?callback=?&client_id=' + clientId, function(streamData) {
-        if (streamData.stream) {
-          var userName = streamData.stream.channel.display_name;
-          var userGame = streamData.stream.channel.game;
-          var userLogo = streamData.stream.channel.logo;
-          var userBanner = streamData.stream.preview.medium;
-          var userStatus = streamData.stream.channel.status;
-          var userUrl = streamData.stream.channel.url;
-          var userViewers = streamData.stream.viewers;
-          console.log("ONLINE :", streamData);
-          console.log("NAME :", userName);
-          console.log("GAME :", userGame);
-          console.log("LOGO :", userLogo);
-          console.log("BANNER :", userBanner);
-          console.log("STATUS :", userStatus);
-          console.log("URL :", userUrl);
-          console.log("VIEWERS :", userViewers);
-
-          onlineUsers.push(obj);
-
-          $('#content').append('\
-    <div class="col-sm-6 col-md-4">\
-    <div class="thumbnail">\
-    <a href="">\
-      <img src="..." alt="...">\
-    </a>\
-        <div class="caption">\
-          <h3></h3>\
-          <h4></h4>\
-          <p class="status">...</p>\
-          <span class="label label-success">Online</span>\
-        </div>\
-    </div>\
-    </div>');
-
-    $('h3').html(userName);
-    $('h4').html(userGame);
-    $('.status').html(userStatus);
-    $('img').attr('src', userLogo);
-    $('a').attr('href', userUrl);
-
+    // get offline users
+    if(streamsData.stream === null) {
+      //console.log(streamsData);
+      $.getJSON(channelsUrl + user + clientId).done(function(channelsData) {
+        //console.log(channelsData);
+        userUrl = channelsData.url;
+        userLogo = channelsData.logo;
+        if(userLogo === null || userLogo === undefined) {
+          userLogo = noLogo;
         }
+        userName = channelsData.display_name;
+        if(userName === undefined) {
+          userName = user;
+        }
+        userGame = channelsData.game;
+        if(userGame === null || userGame === undefined) {
+          userGame = 'NO GAME!!';
+        }
+        userStatus = channelsData.status;
+        spanClass = 'label label-warning';
+        btnLabel = 'Offline';
+        if(userStatus === 404) {
+          userStatus = channelsData.message;
+          spanClass = 'label label-danger';
+          btnLabel = 'Not Found';
+        }
+        //console.log(userStatus);
+         $('#content').append('\
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">\
+            <div class="thumbnail">\
+              <a href="' + userUrl + '" target="_blank">\
+                <img src="' + userLogo + '" alt="...">\
+              </a>\
+              <div class="caption">\
+                <h3>' + userName + '</h3>\
+                <h4>' + userGame + '</h4>\
+                <p class="status">' + userStatus.substring(0,25) + ' ...' + '</p>\
+                <span class="' + spanClass + '">' + btnLabel + '</span>\
+              </div>\
+            </div>\
+          </div>'); 
+      });
+    }
+  }); // end of getJSON
+  }); // end of users.forEach
+} // end of getOfflineData
 
-
-
-
+function getOnlineData() {
+  $('#content').empty();
+  users.forEach(function(user) {
+    $.getJSON(streamsUrl + user + clientId).done(function(streamsData) {
+      console.log(streamsData.stream.channel);
+      if(streamsData.stream !== null && streamsData.stream.channel !== false) {
+        console.log(streamsData.stream);
+        userName = streamsData.stream.channel.display_name;
+        userGame = streamsData.stream.channel.game;
+        userLogo = streamsData.stream.channel.logo;
+        userUrl = streamsData.stream.channel.url;
+        userStatus = streamsData.stream.channel.status;
+        spanClass = 'label label-success';
+            btnLabel = 'Online';
+      }
+    /*  else {
+        userName = streamsData.stream.display_name;
+        userGame = streamsData.stream.game;
+        userLogo = streamsData.stream.logo;
+        userStatus = streamsData.stream.status;
+        spanClass = 'label label-success';
+            btnLabel = 'Online';
+      }*/
+      $('#content').append('\
+        <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">\
+          <div class="thumbnail">\
+            <a href="' + userUrl + '" target="_blank">\
+              <img src="' + userLogo + '" alt="...">\
+            </a>\
+            <div class="caption">\
+              <h3>' + userName + '</h3>\
+              <h4>' + userGame + '</h4>\
+              <p class="status">' + userStatus.substring(0,25) + ' ...' + '</p>\
+              <span class="' + spanClass + '">' + btnLabel + '</span>\
+            </div>\
+          </div>\
+        </div>');
     });
-
-  }
-
-
-});
+  });
+} // end of getOnlineData
