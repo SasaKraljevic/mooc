9d35a6802f3e197a1007a65724456d859febd0b1
salasy <sasa.kraljevic@gmail.com>
2017-04-03 18:48:13 +0200

    Functions complited

--- freeCodeCamp/advancedFrontEndDevelopmentProject/tictactoeGame/js/script.js
+++ freeCodeCamp/advancedFrontEndDevelopmentProject/tictactoeGame/js/script.js
@@ -5,50 +5,32 @@ var u_html = "<i class='fa fa-times fa-3x' aria-hidden='true'></i>";
 var c_html = "<i class='fa fa-circle-o fa-3x' aria-hidden='true'></i>";
 
 var square = [0,0,0,0,0,0,0,0,0];
-var playingGame = true;
+//var playingGame = true;
+var playingGame = false;
 
 var USER = false;
 var COMPUTER = true;
 
 var USERVALUE = -1;
 var COMPUTERVALUE = 1;
-/*
-function choosePlayer(player) {
-    if (player.id != 'playerX') {
-      COMPUTER = false;
-      COMPUTERVALUE = -1;
-      USER = true;
-      USERVALUE = 1;
-      computerPlay();
-
-    }
-}
-*/
-
 
 function chooseX() {
+  playingGame = true;
   u_html;
   c_html;
-  setTimeout(showUserTurn, 200);
-  //resetGame();
-  /*
-  user = false;
-  computer = true;
-  userValue = -1;
-  computerValue = 1;*/
-  //computerPlay();
+  setTimeout(showUserTurn, 150);
 }
 
 function chooseO() {
+  playingGame = true;
   c_html = "<i class='fa fa-times fa-3x' aria-hidden='true'></i>";
   u_html = "<i class='fa fa-circle-o fa-3x' aria-hidden='true'></i>";
 
-  setTimeout(showCompTurn, 200);
+  setTimeout(showCompTurn, 150);
   setTimeout(computerPlay, 1000);
 }
-//////////////
+
 function showUserTurn() {
-  //var userTurn = document.getElementById("userTurn").classList;
   document.getElementById('compTurn').style.visibility = "hidden";
   document.getElementById('compTurn').classList.remove('active');
   document.getElementById('userTurn').style.visibility = "visible";
@@ -56,14 +38,18 @@ function showUserTurn() {
 }
 
 function showCompTurn() {
-
-  //var userTurn = document.getElementById("userTurn").classList;
   document.getElementById('userTurn').style.visibility = "hidden";
   document.getElementById('userTurn').classList.remove('active');
   document.getElementById('compTurn').style.visibility = "visible";
   document.getElementById('compTurn').classList.add('active');
 }
-/////////////
+
+function hideTurns() {
+  document.getElementById('compTurn').style.visibility = "hidden";
+  document.getElementById('compTurn').classList.remove('active');
+  document.getElementById('userTurn').style.visibility = "hidden";
+  document.getElementById('userTurn').classList.remove('active');
+}
 
 var winPositions = [
   [0,1,2],
@@ -77,7 +63,9 @@ var winPositions = [
 ];
 
 function resetGame() {
-  document.getElementsByClassName('message')[0].innerHTML = "";
+  document.getElementsByClassName('message')[0].innerHTML = "..";
+  hideTurns();
+
   for(var x = 0; x < 9; x++) {
     tiles[x].style.background = "#1c3e4a";
     tiles[x].innerHTML = "<i class='fa fa-free-code-camp fa-3x' aria-hidden='true'></i>";
@@ -96,36 +84,25 @@ function makeMove(clickedElement) {
   for(var x = 0; x < 9; x++) {
     if(tiles[x] == clickedElement && square[x] == 0) { // if place is not taken
       play(x, USER);
-      showCompTurn();
+      setTimeout(showCompTurn, 150);
       setTimeout(computerPlay, 1000);
     }
   }
 } // end of makeMove();
 
 function play(index, player) {
-  showUserTurn();
+  setTimeout(showUserTurn, 150);
   if(!playingGame) {
     return;
   }
   if(square[index] == 0) {
-    /*buttons[0].style.width = "0";
-    buttons[0].style.margin = "0";
-    buttons[0].style.opacity = "0";
-    buttons[1].style.width = "32vh";
-*/
-    if(player == USER) {
 
-      //tiles[index].style.background = "#22f";
-      //tiles[index].innerHTML = "<i class='fa fa-times fa-3x' aria-hidden='true'></i>";
+    if(player == USER) {
       tiles[index].innerHTML = u_html;
       square[index] = USERVALUE;
     }
     else{
-
-      //tiles[index].style.background = "#f22";
-      //tiles[index].innerHTML = "<i class='fa fa-circle-o fa-3x' aria-hidden='true'></i>";
       tiles[index].innerHTML = c_html;
-      //square[index] = COMPUTER;
       square[index] = COMPUTER;
     }
     if(isThereWinner(square, player)) {
@@ -147,7 +124,6 @@ function isThereWinner(board, player) {
   else {
   value = COMPUTERVALUE;
   }
-  //var value = player == USER ? USERVALUE : COMPUTERVALUE;
   for(var x = 0; x < 8; x++) {
     var winner = true;
 
@@ -189,25 +165,11 @@ function computerTurn(board, depth, player) {
 
   var value;
   if(player == USER) {
-    //////////////////////
-    //showUserTurn();
-    /*document.getElementById('compTurn').style.visibility = "hidden";
-    document.getElementById('compTurn').classList.remove('active');
-    document.getElementById('userTurn').style.visibility = "visible";
-    document.getElementById('userTurn').classList.add('active');*/
-    //////////////////////
     value = USERVALUE;
   }
   else {
-    //showCompTurn();
-    /*document.getElementById('userTurn').style.visibility = "hidden";
-    document.getElementById('userTurn').classList.remove('active');
-    document.getElementById('compTurn').style.visibility = "visible";
-    document.getElementById('compTurn').classList.add('active');*/
     value = COMPUTERVALUE;
   }
-
-  //var value = player == USER ? USERVALUE : COMPUTERVALUE;
   var max = -Infinity;
   var index = 0;
 
@@ -226,6 +188,5 @@ function computerTurn(board, depth, player) {
   if(depth == 0) {
     play(index, COMPUTER);
   }
-//resetGame();
   return max;
 } // end of computerTurn();
